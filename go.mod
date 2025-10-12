b3a97bbfbbf32957e40d3cc83aed82fc55c08aef
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-10-12 18:28:56 +0800

    import greetings and call its functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhanwang-sky/hello
 
 go 1.25.2
+
+require github.com/zhanwang-sky/greetings v0.0.0-20251012101055-02d43dbeea45
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"github.com/zhanwang-sky/greetings"
 )
 
 const modName string = "hello"
@@ -12,4 +13,5 @@ const pi = 3.1415926
 func main() {
 	fmt.Println("modName:", modName)
 	fmt.Printf("sin(π) = %.0f\n", math.Round(math.Sin(pi)))
+	fmt.Println(greetings.Hello("Jon Snow"))
 }
