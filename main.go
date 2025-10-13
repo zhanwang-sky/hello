afbd6d5007fc3a60dfec33dbf3b04fdff6555555
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-10-13 16:35:01 +0800

    Values & Variables

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"math"
+
 	"github.com/zhanwang-sky/greetings"
 )
 
 const modName string = "hello"
-
 const pi = 3.1415926
 
 func main() {
+	// Values & Variables
+	fmt.Println("Values & Variables:")
+
 	fmt.Println("modName:", modName)
 	fmt.Printf("sin(π) = %.0f\n", math.Round(math.Sin(pi)))
 	fmt.Println(greetings.Hello("Jon Snow"))
+
+	fmt.Println()
 }
