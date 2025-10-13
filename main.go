928dfe75fe9d7a5c0336f086e9a46be291835d98
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-10-13 16:46:25 +0800

    Arrays

--- main.go
+++ main.go
@@ -19,4 +19,32 @@ func main() {
 	fmt.Println(greetings.Hello("Jon Snow"))
 
 	fmt.Println()
+
+	// Arrays
+	fmt.Println("Arrays:")
+
+	var a [5]int
+	fmt.Println("arr a:", a)
+	fmt.Println("length of a:", len(a))
+
+	b := [5]int{5, 4, 3, 2, 1}
+	fmt.Println("arr b:", b)
+
+	b = [...]int{2, 3, 4, 5, 6}
+	fmt.Println("arr b:", b)
+	b[len(b)-1] = 99
+	fmt.Println("the last element of b is", b[len(b)-1])
+
+	c := [...][3]string{
+		{"a", "b", "c"},
+		{"foo", "bar", "balabala"},
+	}
+	for i, row := range c {
+		for j, val := range row {
+			fmt.Printf("c[%d][%d]=%s, ", i, j, val)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println()
 }
